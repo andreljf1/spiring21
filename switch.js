836ee727def6176b2a54da717f59d815225d40de
andreljf1 <aljf1981@gmail.com>
2019-02-21 15:48:26 -0300

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,18 @@
+autenticado = true;
+ 
+switch (autenticado) {
+    case true:
+        console.log("Usuário logado");
+        break;
+    case false:
+        console.log("Usuário não autenticado");
+  case true:
+        console.log("Usuário salvo");
+        break;
+	case false:
+        console.log("Usuário não salvo");	
+		
+		
+  
+		
+		} 
